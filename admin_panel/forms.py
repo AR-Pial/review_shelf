0c4aab8f094bdc452f445c481652c909a53f0e1a
AR-Pial <arpialcodes@gmail.com>
2025-01-18 16:58:12 +0600

    books done except server side data table

--- admin_panel/forms.py
+++ admin_panel/forms.py
@@ -1,6 +1,24 @@
 from django import forms
-from .models import Author, Genre, Type
+from .models import Author, Genre, Type, Book
 
+# Book
+class BookForm(forms.ModelForm):
+    first_release_date = forms.DateField(required=False,widget=forms.DateInput(attrs={'type': 'date'}))
+    latest_release_date = forms.DateField(required=False,widget=forms.DateInput(attrs={'type': 'date'}))
+    description = forms.CharField(required=False,widget=forms.Textarea(attrs={'rows': 4}))
+    prologue = forms.CharField(required=False,widget=forms.Textarea(attrs={'rows': 4}))
+    author = forms.ModelChoiceField(queryset=Author.objects.all(), required=False, widget=forms.Select(attrs={'class': 'select2'}))
+    co_authors = forms.ModelMultipleChoiceField(queryset=Author.objects.all(), required=False, widget=forms.SelectMultiple(attrs={'class': 'select2'}))
+    genre = forms.ModelChoiceField(queryset=Genre.objects.all(), required=False, widget=forms.Select(attrs={'class': 'select2'}),empty_label="None")
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), required=False, widget=forms.SelectMultiple(attrs={'class': 'select2'}))
+    cover_photo = forms.ImageField(required=False,widget=forms.ClearableFileInput(attrs={'class': 'form-control',}),)
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    
+
+# Authorss
 class AuthorForm(forms.ModelForm):
     dob = forms.DateField(required=False,widget=forms.DateInput(attrs={'type': 'date'}))
     bio = forms.CharField(required=False,widget=forms.Textarea(attrs={'rows': 4}))
@@ -19,4 +37,6 @@ class TypeForm(forms.ModelForm):
     class Meta:
         model = Type
         fields = ['name']
+
+
     
