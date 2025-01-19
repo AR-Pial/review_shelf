29b41a10990525b58f09e517d10522af2ff17379
AR-Pial <arpialcodes@gmail.com>
2025-01-19 19:37:24 +0600

    books

--- admin_panel/models.py
+++ admin_panel/models.py
@@ -7,10 +7,10 @@ from django.core.files.storage import default_storage
 
 class Author(TimestampedModel):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255,db_index=True)
     bio = models.TextField(blank=True, null=True)
     dob = models.DateField(blank=True, null=True)
-    country = CountryField(blank=True, null=True)
+    country = CountryField(blank=True, null=True,db_index=True)
     image = models.ImageField(upload_to='authors/', blank=True, null=True) 
 
     class Meta:
@@ -49,12 +49,12 @@ class Type(TimestampedModel):
 
 class Book(TimestampedModel):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
-    isbn = models.CharField(max_length=255, null=True, blank=True)
+    title = models.CharField(max_length=255,db_index=True)
+    isbn = models.CharField(max_length=255, null=True, blank=True,db_index=True)
     cover_photo = models.ImageField(upload_to='book_covers/', blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     prologue = models.TextField(blank=True, null=True)
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name='books',blank=True, null=True)
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name='books',blank=True, null=True,db_index=True)
     co_authors = models.ManyToManyField(Author, related_name='co_authored_books', blank=True)
     first_release_date = models.DateField(blank=True, null=True)
     latest_release_date = models.DateField(blank=True, null=True)
